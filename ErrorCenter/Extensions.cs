5ee1ceea7bb18a14c00292ae3a1233e659f12f03
emoonlit@gmail.com <emoonlit@gmail.com>
2021-08-07 22:29:16 -0300

    Dtos

---
+++ ErrorCenter/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using ErrorCenter.Dtos;
+using ErrorCenter.Models;
+
+namespace ErrorCenter
+{
+    public static class Extensions
+    {
+        public static ErrorDto AsDto(this Error error)
+        {
+            return new ErrorDto
+            {
+                Id = error.Id,
+                Title = error.Title,
+                Description = error.Description,
+                Level = error.Level,
+                EventsCount = error.EventsCount
+            };
+        }
+
+        public static UserDto AsDto(this User user)
+        {
+            return new UserDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+            };
+        }
+    }
+}
